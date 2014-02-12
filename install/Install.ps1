5f51c537e89acc77750c10c3563f2b04e1c00f3d
rikbrowning <rikyb88@gmail.com>
2014-02-12 11:52:49 +0000

    Adding install.ps1
    Install should get chocolatey set up schedule job and down load
    everything

---
+++ install/Install.ps1
@@ -0,0 +1,11 @@
+﻿#Get chocolatey
+Set-ExecutionPolicy unrestricted
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:Path += ";%systemdrive%\chocolatey\bin"
+#Setup scheduled task
+gcm -Module PSScheduledJob
+$dailyTrigger = New-JobTrigger -Daily -At "2:00 PM"
+Unregister-ScheduleJob -Name ChocolateyUpdate -Force
+Register-ScheduledJob -Name ChocolateyUpdate -ScriptBlock {cup all} -Trigger $dailyTrigger
+#install everything
+choco install all -source https://raw.github.com/rikbrowning/RikChocolateySetup/master/packages/packages.config
